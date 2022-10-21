b4fde095725341f34eb6df30a92b9cbc6b62038b
dkwapisz <dkwapisz99@gmail.com>
2022-10-21 23:13:00 +0200

    H2 Database initial config

---
+++ src/main/java/agh/project/scs_forum/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package agh.project.scs_forum.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+    }
+
+}
