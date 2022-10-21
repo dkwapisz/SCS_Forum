029b90c12b973b2128f134a4e287bf739f9a995a
dkwapisz <dkwapisz99@gmail.com>
2022-10-21 22:01:53 +0200

    Initial commit

---
+++ src/main/java/agh/project/scs_forum/ScsForumApplication.java
@@ -0,0 +1,13 @@
+package agh.project.scs_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agh/project/scs_forum/ScsForumApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.project.scs_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
